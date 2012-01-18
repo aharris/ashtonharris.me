1531992595260a9f60c7dd2678bd1e739fd0c02d
Ashton Harris <ahdesignsonline@gmail.com>
2012-01-18 09:44:15 -0500

    added icon to logo

--- header.php
+++ header.php
@@ -1,9 +1,11 @@
 <?php include("head.php"); ?>
 <body>
+
     <header>
        <div class="contentWrap">
          <div id="logo">
            <a href="index.php">
+             <img src="images/logo_icon.png" width="88" height="88" alt="Logo Icon">
              <h1>ashton<span class="darker">harris</span></h1>
              <h2>Web Designer/Front-end Developer</h2>
            </a>
