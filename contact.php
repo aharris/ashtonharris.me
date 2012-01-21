3ef409f26bfd849ae02fd986dde6707f7755483a
Ashton Harris <ahdesignsonline@gmail.com>
2012-01-21 00:19:27 -0500

    tried phph mailers then deleted

--- contact.php
+++ contact.php
@@ -1,9 +1,10 @@
+<?php session_start(); ?>
 <?php $current = 3; ?>
 <?php include("header.php"); ?>
 <div class="contentWrap">
   <div class="indent">
     <div class="content">
-      Contact
+      contact
     </div>
   </div>
 </div>
